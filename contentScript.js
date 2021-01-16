4b801fe87ca70148bee09fbff153ae4492408b0f
lkdfount <lkdfount@uwaterloo.ca>
2021-01-16 10:31:30 -0500

    works to change Trump to ???

--- contentScript.js
+++ contentScript.js
@@ -4,7 +4,5 @@ const regex = new RegExp(FIND, "gi");
 const contents = document.querySelectorAll('a,p,li,span,h1,h2,h3,h4,h5,h6');
 //const contents = document.querySelectorAll('a[href*="http://www.cbc.ca"]');
 for(const snippet of contents){
-    //if (snippet.contains("Trump")){
-        snippet.textContent = snippet.textContent.replace(regex, REPLACE_WITH);
-    //}
+    snippet.textContent = snippet.textContent.replace(regex, REPLACE_WITH);
 }
